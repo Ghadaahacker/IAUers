0cdcbfabb6cb8628b57b3926dce9e4ed9c66068c
Ghadah.G <ighrix79@gmail.com>
2026-04-02 22:07:20 +0300

    Add event listener for edit profile button

--- Grad-Project/Student/JS/profile.js
+++ Grad-Project/Student/JS/profile.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const editProfileBtn = document.getElementById("editProfileBtn");
+
+  editProfileBtn.addEventListener("click", () => {
+    alert("Edit Profile clicked");
+  });
+});
 
