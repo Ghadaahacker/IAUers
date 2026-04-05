0317d036deba72132363c8f5ed60b9c9b239a7b1
Sumaiap <sumaia.ts13@gmail.com>
2026-04-05 21:38:20 +0300

    Update home.js

--- Grad-Project/Admin/JS/home.js
+++ Grad-Project/Admin/JS/home.js
@@ -1,1 +1,16 @@
+const addEventBtn = document.getElementById("addEventBtn");
+const addAnnouncementBtn = document.getElementById("addAnnouncementBtn");
+const editContentBtn = document.getElementById("editContentBtn");
+
+addEventBtn.addEventListener("click", function () {
+  window.location.href = "../HTML/content.html";
+});
+
+addAnnouncementBtn.addEventListener("click", function () {
+  window.location.href = "../HTML/content.html";
+});
+
+editContentBtn.addEventListener("click", function () {
+  window.location.href = "../HTML/content.html";
+});
 
