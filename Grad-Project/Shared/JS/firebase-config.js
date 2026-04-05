3a6060ba38d93a045e9fc375b0a77d0af9a44d71
lama-almuhaysin <lm.almuhaysin@gmail.com>
2026-04-05 18:51:27 +0300

    Update firebase-config.js
    A Firebase configuration and initialization file was created to connect the website with Firebase services including Authentication, Firestore Database, and Storage.

--- Grad-Project/Shared/JS/firebase-config.js
+++ Grad-Project/Shared/JS/firebase-config.js
@@ -1,1 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "iauers.firebaseapp.com",
+  projectId: "iauers",
+  storageBucket: "iauers.firebasestorage.app",
+  messagingSenderId: "278163865940",
+  appId: "1:278163865940:web:9112b4946c387ec39e88c1"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
