5df63f815ddc8854d159510b17974499a7d6f83c
Sumaia Alshehri <sumaia.ts13@email.com>
2026-04-25 00:50:09 +0300

    Add Firebase integration and update script references in Building Management

---
+++ Grad-Project/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSy...",
+  authDomain: "iau-ers.firebaseapp.com",
+  projectId: "iau-ers",
+  storageBucket: "iau-ers.firebasestorage.app",
+  messagingSenderId: "190667321135",
+  appId: "1:190667321135:web:xxxx"
+};
+
+export const app = initializeApp(firebaseConfig);
