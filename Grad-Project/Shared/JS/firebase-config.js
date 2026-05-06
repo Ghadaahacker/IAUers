416264e4bb13e879ca130ea2fb61048e2eb6e451
janaorifej-ui <janaorifej@gmail.com>
2026-05-06 18:30:22 +0300

    Update firebase-config.js

--- Grad-Project/Shared/JS/firebase-config.js
+++ Grad-Project/Shared/JS/firebase-config.js
@@ -1,10 +1,12 @@
+// Firebase imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
+// Firebase config (حق مشروعك)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyChH322jxj-b2_ZCWi71RRrzUG8O7pFkBE",
   authDomain: "iauers.firebaseapp.com",
   projectId: "iauers",
   storageBucket: "iauers.firebasestorage.app",
@@ -12,10 +14,10 @@ const firebaseConfig = {
   appId: "1:278163865940:web:9112b4946c387ec39e88c1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export { auth, db, storage };
+// Export services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
