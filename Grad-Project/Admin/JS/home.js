63d065d179ced79dc3ef1026ca32d9d8bac218f5
Lama-almuhaysin <lm.almuhaysin@gmail.com>
2026-05-11 20:39:26 +0300

    home page update

--- Grad-Project/Admin/JS/home.js
+++ Grad-Project/Admin/JS/home.js
@@ -1,3 +1,10 @@
+import { db } from "../../Shared/JS/firebase-config.js";
+
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 const addEventBtn = document.getElementById("addEventBtn");
 const addAnnouncementBtn = document.getElementById("addAnnouncementBtn");
 const editContentBtn = document.getElementById("editContentBtn");
@@ -14,3 +21,51 @@ editContentBtn.addEventListener("click", function () {
   window.location.href = "../HTML/content.html";
 });
 
+const myEventsCount = document.getElementById("myEventsCount");
+const myAnnouncementsCount = document.getElementById("myAnnouncementsCount");
+const myDraftsCount = document.getElementById("myDraftsCount");
+
+async function loadDashboardStats() {
+
+  const eventsSnapshot =
+    await getDocs(collection(db, "events"));
+
+  const announcementsSnapshot =
+    await getDocs(collection(db, "announcements"));
+
+  let events = 0;
+  let announcements = 0;
+  let drafts = 0;
+
+  eventsSnapshot.forEach((doc) => {
+
+    const data = doc.data();
+
+    if (data.status === "published") {
+      events++;
+    }
+
+    if (data.status === "draft") {
+      drafts++;
+    }
+  });
+
+  announcementsSnapshot.forEach((doc) => {
+
+    const data = doc.data();
+
+    if (data.status === "published") {
+      announcements++;
+    }
+
+    if (data.status === "draft") {
+      drafts++;
+    }
+  });
+
+  myEventsCount.textContent = events;
+  myAnnouncementsCount.textContent = announcements;
+  myDraftsCount.textContent = drafts;
+}
+
+loadDashboardStats();
